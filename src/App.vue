51218cab9d54479f0b047012f5999553c80c001e
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-04-18 00:22:41 +0200

    Add "Website under construction"

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from "./components/HelloWorld.vue";
+import UnderConstruction from "./components/UnderConstruction.vue";
 </script>
 
 <template>
-  <HelloWorld />
+  <UnderConstruction />
 </template>
+
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Lato:wght@400;700;900&display=swap");
+
+* {
+  font-family: "Lato", sans-serif;
+}
+</style>
---
+++ src/components/UnderConstruction.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="under_construction">
+    <h1>Website under construction</h1>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+div.under_construction {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  h1 {
+    text-align: center;
+  }
+}
+</style>
