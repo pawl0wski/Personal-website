6a0e750218b76414e10d0d239ed2ef648def8d9d
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-04-18 00:25:22 +0200

    Add .prettierrc and prettify all files

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,13 @@ import UnderConstruction from "./components/UnderConstruction.vue";
 </script>
 
 <template>
-  <UnderConstruction />
+    <UnderConstruction />
 </template>
 
 <style lang="scss">
 @import url("https://fonts.googleapis.com/css2?family=Lato:wght@400;700;900&display=swap");
 
 * {
-  font-family: "Lato", sans-serif;
+    font-family: "Lato", sans-serif;
 }
 </style>
--- src/components/UnderConstruction.vue
+++ src/components/UnderConstruction.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="under_construction">
-    <h1>Website under construction</h1>
-  </div>
+    <div class="under_construction">
+        <h1>Website under construction</h1>
+    </div>
 </template>
 
 <style lang="scss" scoped>
 div.under_construction {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
 
-  h1 {
-    text-align: center;
-  }
+    h1 {
+        text-align: center;
+    }
 }
 </style>
