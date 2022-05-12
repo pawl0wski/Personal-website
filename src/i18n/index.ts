20c13d62d3717b3bf86f10ccd1bcf9657afaacb3
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-05-12 17:45:02 +0200

    Auto detect language

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -3,8 +3,20 @@ import { createI18n } from "vue-i18n";
 import en from "./lang/en";
 import pl from "./lang/pl";
 
+function detectLanguage(): string {
+    let lang = navigator.language;
+    if (lang.includes("-")) {
+        lang = lang.split("-")[0];
+    }
+    if (!["en", "pl"].includes(lang)) {
+        lang = "en";
+    }
+    return lang;
+}
+
 const i18n = createI18n({
-    locale: "en",
+    locale: detectLanguage(),
+    fallbackLocale: "en",
     messages: {
         en,
         pl,
