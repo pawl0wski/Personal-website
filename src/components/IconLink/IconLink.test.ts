66b674e3984b46621b3da90e3773a43c5a04b8b5
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-05-13 11:14:27 +0200

    Write more tests for IconLink

--- src/components/IconLink/IconLink.test.ts
+++ src/components/IconLink/IconLink.test.ts
@@ -11,4 +11,27 @@ describe("IconLink tests", () => {
         const wrapper = mount(IconLink);
         expect(wrapper.find("a").exists()).toBeFalsy();
     });
+    it("should render if something passed", async () => {
+        const wrapper = mount(IconLink);
+        await wrapper.setProps({ icon: ["a"] });
+        await wrapper.setProps({ href: ["a"] });
+        expect(wrapper.find("a").exists()).toBeTruthy();
+    });
+    it("should inject strings in icon to i class", async () => {
+        const icons = ["a", "b", "c"];
+        const wrapper = mount(IconLink);
+        await wrapper.setProps({ icon: icons });
+
+        const wrapperClasses = wrapper.find("i").classes();
+        expect(wrapperClasses.length === icons.length).toBeTruthy();
+        expect(wrapperClasses.every((e) => icons.includes(e))).toBeTruthy();
+    });
+    it("should redirect to url provided in href prop", async () => {
+        const url = "https://www.example.com";
+        const wrapper = mount(IconLink);
+        await wrapper.setProps({ href: url, icon: ["a"] });
+
+        expect(wrapper.find("a").exists()).toBeTruthy();
+        expect(wrapper.find("a").attributes().href).toBe(url);
+    });
 });
