3daa950eba41ad78245c8f1a746ca736536cf387
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-07-07 18:26:11 +0200

    Add global.scss and change main-color to primary-color

--- components/welcome/components/WelcomeText.vue
+++ components/welcome/components/WelcomeText.vue
@@ -1,5 +1,5 @@
 <template>
     <h1>
-        Cześć, jestem <span class="main-color">Jakub</span>. Zajmuje się <span class="main-color">programowaniem</span> oraz  <span class="main-color">systemami operacyjnymi</span>.
+        Cześć, jestem <span class="primary-color">Jakub</span>. Zajmuje się <span class="primary-color">programowaniem</span> oraz  <span class="primary-color">systemami operacyjnymi</span>.
     </h1>
 </template>
