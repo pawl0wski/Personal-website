2440763fbc764f661acb1dbe9be1c3c448fafc64
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-07-07 22:17:24 +0200

    Add WelcomeCube

--- components/welcome/WelcomeSlide.vue
+++ components/welcome/WelcomeSlide.vue
@@ -3,15 +3,18 @@
         <div class="welcome__left">
             <WelcomeText />
         </div>
-        <div class="welcome__right"></div>
+        <div class="welcome__right">
+            <WelcomeCube />
+        </div>
     </div>
 </template>
 <script>
 import Vue from "vue";
 import WelcomeText from "~/components/welcome/components/WelcomeText";
+import WelcomeCube from "~/components/welcome/components/WelcomeCube";
 
 export default Vue.extend({
-    components: { WelcomeText },
+    components: { WelcomeText, WelcomeCube },
 });
 </script>
 
@@ -20,17 +23,22 @@ div.welcome {
     display: flex;
     width: 100vw;
     height: 100vh;
+    overflow-x: hidden;
 
     &__left,
     &__right {
         width: 50%;
         height: 100%;
+        display: flex;
+        align-items: center;
     }
 
     &__left {
-        display: flex;
-        align-items: center;
         margin: 0 10vw;
+    }
+
+    &__right {
+        justify-content: end;
     }
 }
 </style>
---
+++ components/welcome/components/WelcomeCube.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="welcome__cube">
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    div.welcome__cube {
+        width: 30vw;
+        height: 30vw;
+
+        background: linear-gradient(82.31deg, #FFF01F 5.71%, #5E5700 97.32%);
+        border-radius: 33px;
+        transform: rotate(60deg) translate(5vw, -5vw);
+    }
+</style>
