b94497448983537041608bb29829b629b622f8b3
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-07-07 18:54:03 +0200

    Change text of WelcomeText

--- components/welcome/components/WelcomeText.vue
+++ components/welcome/components/WelcomeText.vue
@@ -1,14 +1,14 @@
 <template>
     <h1>
         Cześć, jestem <span class="primary-color">Jakub</span>. Zajmuje się
-        <span class="primary-color">programowaniem</span> oraz
-        <span class="primary-color">systemami operacyjnymi</span>.
+        <span class="primary-color">frontendem</span> oraz
+        <span class="primary-color">backendem</span>.
     </h1>
 </template>
 
 <style scoped lang="scss">
 h1 {
     line-height: 2.5rem;
-    letter-spacing: 0.1rem;
+    letter-spacing: 0.05rem;
 }
 </style>
