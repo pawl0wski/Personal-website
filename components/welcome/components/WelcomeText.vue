de65c8fe28957ee4e90cb03416b08884f8a20536
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-07-07 18:50:04 +0200

    Change line-height and letter-spacing of WelcomeText

--- components/welcome/components/WelcomeText.vue
+++ components/welcome/components/WelcomeText.vue
@@ -3,3 +3,10 @@
         Cześć, jestem <span class="primary-color">Jakub</span>. Zajmuje się <span class="primary-color">programowaniem</span> oraz  <span class="primary-color">systemami operacyjnymi</span>.
     </h1>
 </template>
+
+<style scoped lang="scss">
+    h1 {
+        line-height: 2.5rem;
+        letter-spacing: 0.1rem;
+    }
+</style>
