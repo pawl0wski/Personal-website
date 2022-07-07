1cc6fe42b49749526380e89aa2cc4ec5da1861f0
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-07-07 18:38:37 +0200

    Add root div.welcome and add css properties for it

--- components/welcome/WelcomeSlide.vue
+++ components/welcome/WelcomeSlide.vue
@@ -1,5 +1,13 @@
 <template>
-    <WelcomeText />
+    <div class="welcome">
+        <div class="welcome__left">
+            <WelcomeText />
+        </div>
+        <div class="welcome__right">
+
+        </div>
+    </div>
+
 </template>
 <script>
 import WelcomeText from "~/components/welcome/components/WelcomeText";
@@ -9,3 +17,18 @@ export default Vue.extend({
     components: {WelcomeText}
 })
 </script>
+
+<style scoped lang="scss">
+
+    div.welcome {
+        display: flex;
+        width: 100vw;
+        height: 100vh;
+
+        &__left, &__right {
+            width: 50%;
+            height: 100%;
+        }
+
+}
+</style>
