f1153f5558216ab0c3d12505ef9026a2938f8566
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-07-07 18:51:29 +0200

    Reformat code

--- components/welcome/WelcomeSlide.vue
+++ components/welcome/WelcomeSlide.vue
@@ -3,38 +3,34 @@
         <div class="welcome__left">
             <WelcomeText />
         </div>
-        <div class="welcome__right">
-
-        </div>
+        <div class="welcome__right"></div>
     </div>
-
 </template>
 <script>
-import WelcomeText from "~/components/welcome/components/WelcomeText";
 import Vue from "vue";
+import WelcomeText from "~/components/welcome/components/WelcomeText";
 
 export default Vue.extend({
-    components: {WelcomeText}
-})
+    components: { WelcomeText },
+});
 </script>
 
 <style scoped lang="scss">
+div.welcome {
+    display: flex;
+    width: 100vw;
+    height: 100vh;
 
-    div.welcome {
+    &__left,
+    &__right {
+        width: 50%;
+        height: 100%;
+    }
+
+    &__left {
         display: flex;
-        width: 100vw;
-        height: 100vh;
-
-        &__left, &__right {
-            width: 50%;
-            height: 100%;
-        }
-
-        &__left {
-            display: flex;
-            align-items: center;
-            margin: 0 10vw;
-        }
-
+        align-items: center;
+        margin: 0 10vw;
+    }
 }
 </style>
--- components/welcome/components/WelcomeText.vue
+++ components/welcome/components/WelcomeText.vue
@@ -1,12 +1,14 @@
 <template>
     <h1>
-        Cześć, jestem <span class="primary-color">Jakub</span>. Zajmuje się <span class="primary-color">programowaniem</span> oraz  <span class="primary-color">systemami operacyjnymi</span>.
+        Cześć, jestem <span class="primary-color">Jakub</span>. Zajmuje się
+        <span class="primary-color">programowaniem</span> oraz
+        <span class="primary-color">systemami operacyjnymi</span>.
     </h1>
 </template>
 
 <style scoped lang="scss">
-    h1 {
-        line-height: 2.5rem;
-        letter-spacing: 0.1rem;
-    }
+h1 {
+    line-height: 2.5rem;
+    letter-spacing: 0.1rem;
+}
 </style>
