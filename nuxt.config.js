73ec9f5039238991a05a767e32aebc369dd26e92
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-07-14 01:42:11 +0200

    Fix mistake from commit 53b5c12

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,11 +11,11 @@ export default {
         meta: [
             { charset: "utf-8" },
             {
-                title: "viewport",
+                name: "viewport",
                 content: "width=device-width, initial-scale=1",
             },
-            { hid: "description", title: "description", content: "" },
-            { title: "format-detection", content: "telephone=no" },
+            { hid: "description", name: "description", content: "" },
+            { name: "format-detection", content: "telephone=no" },
         ],
         link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
     },
