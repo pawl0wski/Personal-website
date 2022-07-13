dc3a7fa6aa548f19743f25f75648d7496e7d3a1e
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-07-14 00:17:29 +0200

    Add InfoPopover

---
+++ components/InfoPopover.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="info-popover">
+        <p class="info-popover__text">{{ content }}</p>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+    props: {
+        content: {
+            type: String,
+            required: true,
+        },
+    },
+});
+</script>
+
+<style lang="scss" scoped>
+#info-popover {
+    padding: 2rem;
+    background-color: $background-color-lighter;
+}
+</style>
