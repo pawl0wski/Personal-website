e2a0fda5f7b424f06c234533c3ab00ff32038451
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-07-14 00:50:21 +0200

    Add shadow to InfoPopover

--- components/InfoPopover.vue
+++ components/InfoPopover.vue
@@ -24,6 +24,7 @@ export default Vue.extend({
     border-radius: 10px;
     background-color: $background-color-lighter;
     opacity: 0;
+    box-shadow: 0px 0px 15px 0px $background-color;
 
     position: absolute;
 
