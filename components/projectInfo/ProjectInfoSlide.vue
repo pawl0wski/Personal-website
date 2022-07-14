a0877cb6b815384beb412b4e319f0484be5f7352
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-07-15 00:12:36 +0200

    Move project page content to ProjectInfoSlide

---
+++ components/projectInfo/ProjectInfoSlide.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="project_info_slide">
+        <a href="/" class="project_info_slide__back">
+            {{ $locale("backToHomePage") }}
+        </a>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({});
+</script>
+
+<style lang="scss" scoped>
+.project_info_slide {
+    &__back {
+        color: $text-color;
+
+        text-decoration: none;
+        font-size: x-large;
+
+        &:hover {
+            color: $primary-color;
+            text-decoration: underline;
+        }
+    }
+}
+</style>
--- pages/project/_projectId.vue
+++ pages/project/_projectId.vue
@@ -1,27 +1,22 @@
 <template>
     <div class="project">
-        <a href="/" class="project__back">
-            {{ $locale("backToHomePage") }}
-        </a>
+        <ProjectInfoSlide />
     </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import ProjectInfoSlide from "~/components/projectInfo/ProjectInfoSlide.vue";
+
+export default defineComponent({
+    components: { ProjectInfoSlide },
+});
+</script>
 
 <style lang="scss" scoped>
 .project {
     @include page;
 
     margin-top: 3rem;
-
-    &__back {
-        color: $text-color;
-
-        text-decoration: none;
-        font-size: x-large;
-
-        &:hover {
-            color: $primary-color;
-            text-decoration: underline;
-        }
-    }
 }
 </style>
