d9cdb4165cf700b48880eb4e64ab84977caa68d6
pawl0wski <59766830+pawl0wski@users.noreply.github.com>
2022-07-16 21:43:01 +0200

    Add currentIndex variable in ProjectInfoImages

--- components/projectInfo/components/ProjectInfoImages.vue
+++ components/projectInfo/components/ProjectInfoImages.vue
@@ -1,11 +1,19 @@
 <template>
-    <div class="project_info_images"></div>
+    <div class="project_info_images">
+        <img />
+    </div>
 </template>
 
 <script>
 import { defineComponent } from "vue";
 
-export default defineComponent({});
+export default defineComponent({
+    data() {
+        return {
+            currentIndex: 0,
+        };
+    },
+});
 </script>
 
 <style lang="scss" scoped>
